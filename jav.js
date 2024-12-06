24f6b2318e9df7dadb3ba68398a139654340c249
名字只是个代号 <48679346+fengtangyuan@users.noreply.github.com>
2024-12-06 21:07:53 +0800

    更新 jav.js

--- jav.js
+++ jav.js
@@ -38,7 +38,7 @@ let appConfig = {
 
 async function getConfig() {
     let config = appConfig
-    await $utils.openSafari(config.site, UA)
+    // await $utils.openSafari(config.site, UA)
     return jsonify(config)
 }
 
