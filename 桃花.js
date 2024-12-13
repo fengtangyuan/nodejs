4bac4a40fc7f3249acdb3251542dee90cf07473f
名字只是个代号 <48679346+fengtangyuan@users.noreply.github.com>
2024-12-13 17:00:17 +0800

    1

--- 桃花.js
+++ 桃花.js
@@ -6,20 +6,19 @@ let appConfig = {
     ver: 1,
     title: '桃花族',
     // 40thz.com
-    site: 'http://7340hsck.cc',
+    site: 'http://7343hsck.cc',
 }
 
 async function getConfig() {
     let config = appConfig
-    const response = await $fetch.get(`https://888tttz.com:8899/?u=${config.site}/&p=/`, {
-        headers: {
-            'User-Agent': UA,
-
-        },
-    })
-    const host = response.request.host
-    const realurl = `http://${host}`
-    config.site = realurl
+    //const response = await $fetch.get(`https://888tttz.com:8899/?u=${config.site}/&p=/`, {
+    //    headers: {
+    //        'User-Agent': UA,
+    //    },
+    //})
+    //const host = response.request.host
+    //const realurl = `http://${host}`
+    //config.site = realurl
     config.tabs = await getTabs()
     return jsonify(config)
 }
