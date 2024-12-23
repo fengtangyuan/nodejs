5509fca4b249b291a387d0c7b0020d8be250e8f5
名字只是个代号 <48679346+fengtangyuan@users.noreply.github.com>
2024-12-23 14:17:02 +0800

    1

--- missav.js
+++ missav.js
@@ -1,4 +1,4 @@
-const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
+const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 Edg/131.0.0.0'
 const cheerio = createCheerio()
 
 const appConfig = {
