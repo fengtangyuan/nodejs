843cff4eabf6a1f1756674c556e2c98b4bc06deb
名字只是个代号 <48679346+fengtangyuan@users.noreply.github.com>
2024-12-31 12:31:19 +0800

    1

--- hanime.js
+++ hanime.js
@@ -68,7 +68,7 @@ async function getCards(ext) {
     if (videolist.length === 0) videolist = $('.content-padding-new > .row > .search-doujin-videos.col-xs-6')
 
     videolist.each((_, element) => {
-        if ($(element).attr('target') == '_blank') return
+        if ($(element).attr('target') === '_blank') return
         const href = $(element).attr('href') || $(element).find('.overlay').attr('href')
         const title = $(element).find('.home-rows-videos-title').text() || $(element).find('.card-mobile-title').text()
         let cover = $(element).find('img').attr('src')
@@ -163,7 +163,7 @@ async function search(ext) {
     const $ = cheerio.load(data)
 
     $('.col-xs-6').each((_, element) => {
-        if ($(element).attr('target') == '_blank') return
+        if ($(element).attr('target') === '_blank') return
         const href = $(element).find('.overlay').attr('href')
         const title = $(element).find('.card-mobile-title').text()
         const cover = $(element).find('img').eq(1).attr('src')
