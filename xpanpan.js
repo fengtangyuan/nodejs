d18bdefefa783e257a6b23f7797f2daa83ee469b
名字只是个代号 <48679346+fengtangyuan@users.noreply.github.com>
2025-01-09 15:46:13 +0800

    1

--- xpanpan.js
+++ xpanpan.js
@@ -1,4 +1,4 @@
-const UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
+const UA = "Mozilla/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit/604.1.14 (KHTML, like Gecko)"
 const cheerio = createCheerio()
 
 const appConfig = {
