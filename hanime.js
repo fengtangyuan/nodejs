5fe35c74df5ed311e50e101e7b52ed4c532f9158
名字只是个代号 <48679346+fengtangyuan@users.noreply.github.com>
2025-04-24 10:19:20 +0800

    1

--- hanime.js
+++ hanime.js
@@ -1,5 +1,3 @@
-const { all } = require("axios")
-
 const cheerio = createCheerio()
 
 const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
