a8e8b9cca1b29108fbf7e754e089a7dabd98499f
名字只是个代号 <48679346+fengtangyuan@users.noreply.github.com>
2025-07-18 12:20:02 +0800

    1

--- dashixiong.js
+++ dashixiong.js
@@ -3,45 +3,48 @@ const CryptoJS = createCryptoJS()
 
 const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0'
 
+
 let $config = argsify($config_str)
 if (!$config) {
-    $config = [
-        {
-            name: '电影',
-            ui: 1,
-            ext: {
-                id: '/vodshow/1--time------p---',
+    $config = {
+        tabs: [
+            {
+                name: '电影',
+                ui: 1,
+                ext: {
+                    id: '/vodshow/1--time------p---',
+                },
             },
-        },
-        {
-            name: '电视剧',
-            ui: 1,
-            ext: {
-                id: '/vodshow/2--time------p---',
+            {
+                name: '电视剧',
+                ui: 1,
+                ext: {
+                    id: '/vodshow/2--time------p---',
+                },
             },
-        },
-        {
-            name: '综艺',
-            ui: 1,
-            ext: {
-                id: '/vodshow/3--time------p---',
+            {
+                name: '综艺',
+                ui: 1,
+                ext: {
+                    id: '/vodshow/3--time------p---',
+                },
             },
-        },
-        {
-            name: '动漫',
-            ui: 1,
-            ext: {
-                id: '/vodshow/4--time------p---',
+            {
+                name: '动漫',
+                ui: 1,
+                ext: {
+                    id: '/vodshow/4--time------p---',
+                },
             },
-        },
-    ]
+        ]
+    }
 }
 
 let appConfig = {
     ver: 1,
     title: '大师兄',
     site: 'https://dsxys.com',
-    tabs: $config
+    tabs: $config.tabs
 }
 
 async function getConfig() {
