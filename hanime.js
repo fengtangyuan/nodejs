6454cda5c878959af838c8f06263f951eca41c78
名字只是个代号 <48679346+fengtangyuan@users.noreply.github.com>
2025-10-09 15:12:16 +0800

    1

--- hanime.js
+++ hanime.js
@@ -28,7 +28,7 @@ async function getTabs() {
     })
 
     const $ = cheerio.load(data)
-    let allClass = $('#sub-nav-home-mobile > a.home-genre-tabs')
+    let allClass = $('home-genre-tabs-wrapper > a')
 
     allClass.each((i, e) => {
         const name = $(e).text()
